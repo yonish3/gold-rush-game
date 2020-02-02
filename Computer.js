972c49e354a7e31ce00c92c0c8ebcbb7bd2f3664
Yoni <yonish3@gmail.com>
2020-02-02 18:26:06 +0200

    i dont really know why

---
+++ Computer.js
@@ -0,0 +1,91 @@
+const copyMatrix = function(matrix, pathSoFar){
+    let matrixCopy = []
+        
+    for (let r = 0; r < matrix.length; r++) {
+        matrixCopy.push([])
+        for (let c = 0; c < matrix[r].length; c++) {
+            matrixCopy[r].push(matrix[r][c])
+        }
+    }
+
+    for (let i = 0; i < pathSoFar.length; i++) {
+        matrixCopy[pathSoFar[i].row][pathSoFar[i].col] = '$'
+    }
+
+    return matrixCopy
+}
+
+
+const computer = function(GoldRushBoard){
+    let rowM = GoldRushBoard.row
+    let colM = GoldRushBoard.col
+    let pathSoFar = [{row:0,col:0}]
+    let flag = true
+    
+    while (flag) {
+        let matrixCopy = copyMatrix(GoldRushBoard.matrix, pathSoFar)
+        let path = [...GoldRushBoard.compPath]
+        GoldRushBoard.compPath = []
+        GoldRushBoard.checkPath(path[1].row, path[1].col, rowM, colM, matrixCopy)
+        
+        GoldRushBoard.alter(path[0].row, path[0].col,'.')
+        GoldRushBoard.alter(path[1].row, path[1].col,1)
+
+        Render.generateMatrix(5, 5, GoldRushBoard.matrix)
+        pathSoFar.push({
+            row: path[1].row, 
+            col: path[1].col
+        })
+     }    
+}
+
+
+
+// const copyPath = function(matrixCopy, pathArray){
+//     for (let i = 0; i <= pathArray.length-1; i++) {
+//         let row = pathArray[i].row
+//         let col = pathArray[i].col
+//         matrixCopy[row][col] = '$'
+
+//     }
+// }
+
+// const computer = function (rowS, colS, rowM, colM, GoldRushBoard, matrixCopy, counter, updatePath){
+
+//     if (GoldRushBoard.player1.score == 10 || GoldRushBoard.player2.score == 10) return
+  
+    
+//     setTimeout(function(){ 
+    
+//     updatePath[counter] = GoldRushBoard.compPath[counter]
+//     updatePath[counter+1] = GoldRushBoard.compPath[counter+1]
+
+//     matrixCopy = copyMatrix(GoldRushBoard.matrix)
+//     copyPath(matrixCopy, updatePath)
+        
+//     row = updatePath[counter].row
+//     col = updatePath[counter].col
+//     GoldRushBoard.alter(row,col,'.')
+
+//     nextRow = updatePath[counter+1].row
+//     nextCol = updatePath[counter+1].col
+//     GoldRushBoard.alter(nextRow, nextCol, 1)
+
+//     Render.generateMatrix(rowM, colM, GoldRushBoard.matrix)
+
+
+//     //GoldRushBoard.compPath.splice(updatePath.length, updatePath.length - 1)
+//     //updatePath.splice(counter+1, updatePath.length - 1)
+//     counter++
+    
+//     GoldRushBoard.compPath=[]
+//     GoldRushBoard.checkPath(nextRow,nextCol,rowM-1, colM-1, matrixCopy)
+
+    
+//     computer(nextRow, nextCol, rowM, colM, GoldRushBoard, matrixCopy, counter)
+
+    
+//     }, 1000)
+
+// }
+
